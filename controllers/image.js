0bad331699a3874e30144fb8147bab55a2126a9a
Asad-Sanuri <asad.sanuri@gmail.com>
2021-10-04 18:30:19 +0200

    test

--- controllers/image.js
+++ controllers/image.js
@@ -19,7 +19,12 @@ const handleImage = (req, res, db) => {
     .increment('entries', 1)
     .returning('entries')
     .then(entries => {
-      res.json(entries[0]);
+      if(entries.length){
+        res.json(entries[0]);
+      } else {
+        res.status(400).json("unable to get entries of user with id = " + id);        
+      }
+      
     })
     .catch(err => res.status(400).json('unable to get entries'))
   }
