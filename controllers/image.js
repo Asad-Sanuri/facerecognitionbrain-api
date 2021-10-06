0d3cb8bde85566f7e8fbab779ad463e2ae37fe7f
Asad-Sanuri <asad.sanuri@gmail.com>
2021-10-06 20:38:32 +0200

    test

--- controllers/image.js
+++ controllers/image.js
@@ -29,11 +29,11 @@ const handleImage = (req, res, db) => {
   .catch(err => res.status(400).json('unable to get entries'))
 }
 
-cloudinary.config({ 
+cloudinary.config({
+  secure: true, 
   cloud_name: process.env.CLOUDINARY_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true
+  api_secret: process.env.CLOUDINARY_API_SECRET  
 });
 
 const handleImageUpload = () => (req, res) => {
